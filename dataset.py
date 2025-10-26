c455258c948b155af5e823222c27279f368a657f
Marina Wang <marina44@bu.edu>
2025-10-26 08:09:59 -0400

    Formated Data (Cleaning)

---
+++ dataset.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df= pd.read_csv("coffee_shop_revenue.csv")
+
+df= df.dropna()
+df.columns = [
+    "Number of Customers Per Day",
+    "Average Order Value",
+    "Operating Hours Per Day",
+    "Number of Employees",
+    "Marketing Spend Per Day",
+    "Location Foot Traffic",
+    "Daily Revenue"
+]
+df.to_csv("coffee_shops.csv", index=False)
